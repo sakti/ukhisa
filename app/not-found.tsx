11cb0ffe8b95a0ace43ab60e2c6bd6d1b574d3b5
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2024-09-21 10:23:08 +0700

    set runtime to edge in not found
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+export const runtime = "edge";
+
 export default function NotFound() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
